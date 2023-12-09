d9178c753069f0734e1d24fc7f0f188f1a40eedf
lucamarchiori <luca.marchiori@hotmail.it>
2023-12-09 09:43:47 +0100

    Logging refactoring

--- cmd/scanner/main.go
+++ cmd/scanner/main.go
@@ -13,9 +13,18 @@ import (
 
 var log = logrus.New()
 
-func main() {
+func init() {
+	// Setting the logger to a file
 	log.Out = os.Stdout
+	file, err := os.OpenFile("btlog.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		log.Out = file
+	} else {
+		log.Info("Failed to log to file, using default stderr")
+	}
+}
 
+func main() {
 	log.Infof("Bluetooth affluence script started")
 
 	aid := adapter.GetDefaultAdapterID()

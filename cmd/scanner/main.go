df26284f695199d0ceca0b7552522f1ba610b936
lucamarchiori <luca.marchiori@hotmail.it>
2023-12-09 09:49:55 +0100

    Log refactoring

--- cmd/scanner/main.go
+++ cmd/scanner/main.go
@@ -16,12 +16,12 @@ var log = logrus.New()
 func init() {
 	// Setting the logger to a file
 	log.Out = os.Stdout
-	file, err := os.OpenFile("btlog.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err == nil {
-		log.Out = file
-	} else {
-		log.Info("Failed to log to file, using default stderr")
-	}
+	// file, err := os.OpenFile("./btlog.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	// if err == nil {
+	// 	log.Out = file
+	// } else {
+	// 	log.Info("Failed to log to file, using default stderr")
+	// }
 }
 
 func main() {
